b56862aade4cd33c0571f73b2a6806fe8fcf9527
ceit9802 <ceit9802@gmail.com>
2020-11-03 07:07:35 -0500

    formatting

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
@@ -10,16 +10,16 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-const store =configureStore();
+const store = configureStore();
 store.subscribe(() => {
   const state = store.getState();
   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
   console.log(visibleExpenses);
 });
 
-const jsx =(
+const jsx = (
   <Provider store={store}>
-  <AppRouter />
+    <AppRouter />
   </Provider>
-)
+);
 ReactDOM.render(jsx, document.getElementById('app'));
